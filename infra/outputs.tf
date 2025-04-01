b8de272b58056f7ea38b184cb1429cc5465ac494
Victor Cleber <13708226+victor-cleber@users.noreply.github.com>
2025-04-02 12:40:29 +1300

    Setup RDS

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,3 +1,17 @@
 output "vpc_arn" {
   value = aws_vpc.main.arn
 }
+
+output "rds_endpoint" {
+  value = aws_db_instance.db_instance.endpoint
+}
+
+output "rds_username" {
+  value     = aws_db_instance.db_instance.username
+  sensitive = true
+}
+
+output "rds_password" {
+  value     = aws_db_instance.db_instance.password
+  sensitive = true
+}
